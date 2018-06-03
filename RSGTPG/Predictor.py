df926b0435d3e56429c2f088d2c23622a54ab889
fflor <fflores.du@gmail.com>
2018-06-03 15:39:56 -0700

    Predict from multiple files
    train regressor on each telemetry file
    return mean prediction

--- RSGTPG/Predictor.py
+++ RSGTPG/Predictor.py
@@ -11,36 +11,39 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
+import mapper as map
 
 class Predictor():
     #creates linear regression model
-    def train(self):
-       
+    def train(self, filekey):
+
         # Importing the dataset
-        dataset = pd.read_csv('..\\data\\tele.csv')
+        mapr = map.Mapper()
+        dataset = mapr.get_entry(filekey)
+        #dataset = pd.read_csv('..\\data\\tele.csv')
         #dataset = pd.read_parquet('..\\data\\data.parquet.gz')
-        
+
         #convert Time to time since the start
-        dataset["Time"] = pd.to_datetime(dataset["Time"])
-        dataset["Time"] = (dataset["Time"]- dataset["Time"].min()) / np.timedelta64(1,'m')
-        
+        dataset['Time'] = pd.to_datetime(dataset['Time'])
+        dataset['Time'] = (dataset['Time']- dataset['Time'].min()) / np.timedelta64(1,'m')
+
         # Taking care of missing data
-        filter = (dataset["Latitude"].notna()) & (dataset["Longitude"].notna()) & (dataset["Time"].notna())
+        filter = (dataset['Latitude'].notna()) & (dataset['Longitude'].notna()) & (dataset['Time'].notna())
         dataset = dataset[filter]
-        
+
         #set x to location and y to time
         x = dataset.iloc[:, 1:3].values
         y = dataset.iloc[:, 0].values
-        
+
         # Splitting the dataset into the Training set and Test set
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
-        
+
         # Fitting Multiple Linear Regression to the Training set
         regressor = LinearRegression()
         regressor.fit(x_train, y_train)
-         
-      
-        
+
+
+
         #plot
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -49,17 +52,24 @@ class Predictor():
         return regressor
     # Predicting the Test set results
     def predict(self, lat, long, time_begin):
-        regressor = self.train()
-        #take the lat long and plug it into the model
-        destination = [[lat,long]]
-        #get the predited time from start
-        prediction = regressor.predict(destination)
-        
+        training_set = []
+        for i in range(1,15):
+            training_set.append('tele'+str(i))
+        predictions = []
+        for file in training_set:
+            regressor = self.train(file)
+            #take the lat long and plug it into the model
+            destination = [[lat,long]]
+            #get the predited time from start
+            predictions.append(regressor.predict(destination))
+        #average the predictions
+        prediction = np.mean(predictions)
         #add prediction to time_begin
         predicted_time = pd.to_datetime(time_begin) + np.timedelta64(int(prediction[0]), 'm')
         #return time
         return predicted_time
-        
 
 
-    
+
+
+
