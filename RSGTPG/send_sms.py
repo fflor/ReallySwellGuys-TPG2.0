c321606478cd04367064d346d3fc274aa74efb17
fflor <fflores.du@gmail.com>
2018-06-03 13:52:26 -0700

    resync

---
+++ RSGTPG/send_sms.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import configparser
+import os
+from twilio.rest import Client
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('../config/example.ini')
+
+print(config.sections())
+
+account_sid = config['Twilio'].get('account_sid')
+auth_token = config['Twilio'].get('auth_token')
+
+client = Client(account_sid, auth_token)
+
+'''
+client.messages.create(
+        to=config['PhoneNum'].get('twilio_num'),
+        from_=config['PhoneNum'].get('customer_num'),
+        body="Hello. Your container will be picked up at [Time] at [Address]."
+        )
+'''
